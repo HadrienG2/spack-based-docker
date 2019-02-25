a0e3a13506642ef505bd6c6639469467f1e126bc
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-25 16:34:29 +0100

    Tune down our delta-debugging expectations

--- acts-verrou/run.sh
+++ acts-verrou/run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 OUTPUT_DIR="$1"
-
-# These tests are excluded beceause they either take too long or freeze
-# indefinitely when run in verrou without any active exclusion rule.
-EXCLUDE_REGEX="^(SolenoidBField|MaterialCollection|Extrapolator|LoopProtection)"
-
-cd ${ACTS_BUILD_DIR}
-valgrind --tool=verrou                                                         \
-         --rounding-mode=farthest                                              \
-         --demangle=no                                                         \
-         --trace-children=yes                                                  \
-         ctest -j8 -E ${EXCLUDE_REGEX}
-echo $?> ${OUTPUT_DIR}/results.dat
+TEST="${ACTS_BUILD_DIR}/Tests/Core/Tools/SurfaceArrayCreatorTests"
+valgrind --tool=verrou --rounding-mode=random --demangle=no ${TEST} > ${OUTPUT_DIR}/results.dat
