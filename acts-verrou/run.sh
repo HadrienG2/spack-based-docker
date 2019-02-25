eb8996960afa076c5bacb88f9673b1f26882a801
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-25 14:59:46 +0100

    Try a new delta-debugging process

--- acts-verrou/run.sh
+++ acts-verrou/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-DIR="$1"
-WORKDIR="${ACTS_BUILD_DIR}/Tests/Integration"
-valgrind --tool=verrou --rounding-mode=random --demangle=no $WORKDIR/PropagationTests > ${DIR}/results.dat
+OUTPUT_DIR="$1"
+
+# These tests are excluded beceause they either take too long or freeze
+# indefinitely when run in verrou without any active exclusion rule.
+EXCLUDE_REGEX="^(SolenoidBField|MaterialCollection|Extrapolator|LoopProtection)"
+
+cd ${ACTS_BUILD_DIR}
+valgrind --tool=verrou                                                         \
+         --rounding-mode=farthest                                              \
+         --demangle=no                                                         \
+         --trace-children=yes                                                  \
+         ctest -j8 -E ${EXCLUDE_REGEX}                                         \
+         > ${OUTPUT_DIR}/results.dat
