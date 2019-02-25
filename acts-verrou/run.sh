54f8c735d19bbda94b2c77a6510ccc488efbf9b8
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-25 16:53:11 +0100

    Can't use unit test output for comparison, Boost puts timestamps in it

--- acts-verrou/run.sh
+++ acts-verrou/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 OUTPUT_DIR="$1"
 TEST="${ACTS_BUILD_DIR}/Tests/Core/Tools/SurfaceArrayCreatorTests"
-valgrind --tool=verrou --rounding-mode=random --demangle=no ${TEST} > ${OUTPUT_DIR}/results.dat
+valgrind --tool=verrou --rounding-mode=random --demangle=no ${TEST}
+echo $? > ${OUTPUT_DIR}/results.dat
