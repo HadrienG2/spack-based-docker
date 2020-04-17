ce20f73796a03a17a03d7071e664d6770fcf16c4
Hadrien G <knights_of_ni@gmx.com>
2020-04-17 14:40:50 +0200

    Update to Acts v0.22

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
 ROOT_VERSION="6.20.02"
-ACTS_VERSION="0.21.00"
+ACTS_VERSION="0.22.00"
 ACTS_BUILD_TYPES=(
     "Debug"
     "RelWithDebInfo"
