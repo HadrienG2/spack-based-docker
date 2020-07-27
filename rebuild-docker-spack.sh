63d75b5a9ea75b21ffbbf739f381678c48210e2c
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-07-27 11:11:59 +0200

    Move to acts v0.29

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
 ROOT_VERSION="6.22.00"
-ACTS_VERSION="0.28.00"
+ACTS_VERSION="0.29.00"
 ACTS_BUILD_TYPES=(
     "Debug"
     "RelWithDebInfo"
