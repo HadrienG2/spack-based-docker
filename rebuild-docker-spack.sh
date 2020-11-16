e5088c14c2b34220de10bb96aca3a35aab3e812a
Hadrien G <knights_of_ni@gmx.com>
2020-11-16 10:03:34 +0100

    Adapt to Acts v2

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
 ROOT_VERSION="6.22.00"
-ACTS_VERSION="1.2.0"
+ACTS_VERSION="2.0.0"
 ACTS_BUILD_TYPES=(
     "Debug"
     "RelWithDebInfo"
