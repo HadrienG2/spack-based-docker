6c75341edcc3c03519f81dc5d67f4c23a41bb2ee
Hadrien G <knights_of_ni@gmx.com>
2021-05-31 16:23:45 +0200

    Move to ACTS v8.3

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
 ROOT_VERSION="6.24.00"
-ACTS_VERSION="8.2.0"
+ACTS_VERSION="8.3.0"
 ACTS_BUILD_TYPES=(
     "Debug"
     "RelWithDebInfo"
