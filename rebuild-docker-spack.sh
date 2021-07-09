eccc3df050ec902e56307d0d742bc258b8791bf1
Hadrien G <knights_of_ni@gmx.com>
2021-07-09 21:12:34 +0200

    Update ROOT

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 # Build parameters
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
-ROOT_VERSION="6.24.00"
+ROOT_VERSION="6.24.02"
 ACTS_VERSION="9.1.0"
 ACTS_BUILD_TYPES=(
     "Debug"
