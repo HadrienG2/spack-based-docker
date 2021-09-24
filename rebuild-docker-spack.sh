fe8d0c16ff4b6c0b297a288097888e0b419c3b9c
Hadrien Grasland <grasland@lal.in2p3.fr>
2021-09-24 17:49:04 +0200

    Move to new ACTS version, try to enable SYCL

--- rebuild-docker-spack.sh
+++ rebuild-docker-spack.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOCKER_REPO="hgrasland"
 VERROU_VERSION="2.2.0"
 ROOT_VERSION="6.24.06"
-ACTS_VERSION="12.0.1"
+ACTS_VERSION="13.0.0"
 ACTS_BUILD_TYPES=(
     "Debug"
     "RelWithDebInfo"
